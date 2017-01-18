8c16fb68a40a6c62076f7068d7e9267626b0d6e0
izabera <izaberina@gmail.com>
2017-01-18 23:29:22 +0100

    use the right size in malloc

--- lib/stdlib/malloc.c
+++ lib/stdlib/malloc.c
@@ -2,6 +2,6 @@
 
 void *malloc(size_t s) {
   max_align_t *x = mmap(NULL, s+sizeof(max_align_t), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-  if (x) x++->s = s;
+  if (x) x++->s = s+sizeof(max_align_t);
   return x;
 }

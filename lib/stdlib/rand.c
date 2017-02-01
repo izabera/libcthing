5f47f74ff470bd79c49b1b16f15a3d5117289eb4
izabera <izaberina@gmail.com>
2017-02-01 05:46:17 +0100

    add fake rand48 functions

--- lib/stdlib/rand.c
+++ lib/stdlib/rand.c
@@ -12,16 +12,18 @@ static uint32_t u32_rand(pcg32_random_t* rng) {
   return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
 
-enum { RAND, RANDOM_HI, RANDOM_LO, PRIVATE };
+enum { RAND, RANDOM_HI, RANDOM_LO, RAND48, PRIVATE };
 
 static pcg32_random_t states[] = {
   [RAND]      = { .state = 1, .inc = 123456789987654321l },
   [RANDOM_HI] = { .state = 1, .inc = 112233445566778899l },
   [RANDOM_LO] = { .state = 1, .inc = 123456789123456789l },
+  [RAND48]    = { .state = 1, .inc = 192837465546372819l },
   [PRIVATE]   = { .state = 1, .inc = 0x1234567890abcdefl },
 };
 
 unsigned __private_rand(void) { return u32_rand(&states[PRIVATE]); }
+void __private_srand(unsigned long s) { states[PRIVATE].state = s; }
 
 
 
@@ -57,7 +59,7 @@ void srand(unsigned int seed) { states[RAND].state = seed; }
 
 long random(void) {
   union {
-    struct { unsigned int i[2]; };
+    unsigned int i[2];
     unsigned long u;
   } u = {
     .i[0] = u32_rand(&states[RANDOM_HI]),
@@ -84,3 +86,58 @@ char *setstate(char *state) {
   char *ret = oldstate;
   return ret;
 }
+
+
+
+
+// *rand48 are just implemented as pcg generators
+// this is not posix but it probably won't break anything
+// pcg generators are better anyway
+
+long lrand48(void) { return u32_rand(&states[RAND48]) & INT_MAX; }
+long mrand48(void) { return (int)u32_rand(&states[RAND48]); }
+
+long nrand48(unsigned short seed[3]) {
+  states[RAND48].state = 0;
+  __builtin_memcpy(&states[RAND48].state, seed, 6);
+  return lrand48();
+}
+
+long jrand48(unsigned short seed[3]) {
+  states[RAND48].state = 0;
+  __builtin_memcpy(&states[RAND48].state, seed, 6);
+  return mrand48();
+}
+
+void srand48(long int seed) { states[RAND48].state = seed; }
+
+
+
+// this is the same as what musl, uclibc and glibc do:
+// create a double in [1,2) and then scale it down
+
+double drand48(void) {
+  pcg32_random_t tmp = { .state = states[RAND48].state, .inc = 123456789 };
+  union {
+    double d;
+    unsigned int ui[2];
+    unsigned long ul;
+  } u = {
+    .ui[0] = u32_rand(&tmp),
+    .ui[1] = u32_rand(&states[RAND48])
+  };
+  u.ul &=     0xfffffffffffful;
+  u.ul |= 0x3ff0000000000000ul; // 1.0
+  return u.d - 1.0;
+}
+
+double erand48(unsigned short seed[3]) {
+  states[RAND48].state = 0;
+  __builtin_memcpy(&states[RAND48].state, seed, 6);
+  return drand48();
+}
+
+
+
+// and this just throws 6 bytes away
+void lcong48(unsigned short seed[7]) { __builtin_memcpy(&states[RAND48].state, seed, 8); }
